5ee7edab064735810c625e869ee30fc0eac0dd5f
Jose Duarte <saert@outlook.com>
2024-04-15 21:01:26 -0400

    Create brush component for svg

--- components/hero/index.vue
+++ components/hero/index.vue
@@ -1,6 +1,7 @@
 <script setup>
 import Header from "./Header.vue";
 import LineSVG from "../global/svg/LineSVG.vue";
+import BrushSVG from "../global/svg/BrushSVG.vue";
 </script>
 
 <template>
@@ -34,59 +35,6 @@ import LineSVG from "../global/svg/LineSVG.vue";
           >
             Find out more
           </button>
-          <button type="button" aria-label="Play demo" class="font-poppins flex mt-5">
-            <svg
-              class="w-20"
-              viewBox="0 0 112 112"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <g filter="url(#filter0_d_1_131)">
-                <circle cx="56" cy="41" r="26" fill="#DF6951" />
-              </g>
-              <path
-                d="M64 42.5L52.75 48.9952L52.75 36.0048L64 42.5Z"
-                fill="white"
-              />
-              <defs>
-                <filter
-                  id="filter0_d_1_131"
-                  x="0"
-                  y="0"
-                  width="112"
-                  height="112"
-                  filterUnits="userSpaceOnUse"
-                  color-interpolation-filters="sRGB"
-                >
-                  <feFlood flood-opacity="0" result="BackgroundImageFix" />
-                  <feColorMatrix
-                    in="SourceAlpha"
-                    type="matrix"
-                    values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-                    result="hardAlpha"
-                  />
-                  <feOffset dy="15" />
-                  <feGaussianBlur stdDeviation="15" />
-                  <feColorMatrix
-                    type="matrix"
-                    values="0 0 0 0 0.87451 0 0 0 0 0.411765 0 0 0 0 0.317647 0 0 0 0.3 0"
-                  />
-                  <feBlend
-                    mode="normal"
-                    in2="BackgroundImageFix"
-                    result="effect1_dropShadow_1_131"
-                  />
-                  <feBlend
-                    mode="normal"
-                    in="SourceGraphic"
-                    in2="effect1_dropShadow_1_131"
-                    result="shape"
-                  />
-                </filter>
-              </defs>
-            </svg>
-            <p class="mt-5">Play Demo</p>
-          </button>
         </div>
       </div>
       <div>
@@ -97,17 +45,7 @@ import LineSVG from "../global/svg/LineSVG.vue";
           alt="travel-girl"
           sizes="sm:100vw md:50vw lg:400px"
         />
-        <svg
-          class="absolute top-0 right-0 w-[35rem] -z-10"
-          viewBox="0 0 704 724"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M22.3069 222C-18.0931 153.2 5.4736 13.3333 22.3069 -48L701.807 -149L766.307 122.5L727.807 723.5C649.14 724.5 473.707 717 401.307 679C310.807 631.5 351.807 555 276.807 515C201.807 475 244.807 369.5 218.807 329.5C192.807 289.5 72.8069 308 22.3069 222Z"
-            fill="#FFF1DA"
-          />
-        </svg>
+        <BrushSVG />
       </div>
     </section>
   </div>
