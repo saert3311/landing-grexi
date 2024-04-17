9efce0aedeeddcc7de389063cdde729bbc2d0d50
Jose Duarte <saert@outlook.com>
2024-04-17 12:58:46 -0400

    Footer component

---
+++ components/global/SocialButton.vue
@@ -0,0 +1,10 @@
+<template>
+  <a :href="props.href" class="text-gray-500 hover:text-gold" target="_blank" rel="noreferer noopener nofollow">
+    <slot />
+  </a>
+</template>
+<script setup>
+const props = defineProps({
+  href: String,
+});
+</script>
